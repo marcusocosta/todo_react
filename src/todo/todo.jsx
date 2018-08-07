c914e76e68ae2bfc8cb2dd8a16de91c0e1f6982b
Marcus Costa <marcus.costa@minutrade.com>
2018-08-06 21:12:37 -0300

    Criando o repositório

---
+++ src/todo/todo.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <h1>TODO</h1>
+      </div>
+    )
+  }
+}
